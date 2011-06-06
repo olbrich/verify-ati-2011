0844361ae524866c303126ff19fd80f7b0acfeb9
Kevin Olbrich <kolbrich@icontact.com>
2011-06-05 20:49:08 -0400

    basic configs, setup rake task to run tests

--- rakefile.rb
+++ rakefile.rb
@@ -5,3 +5,8 @@ desc "Run browser based tests using Watir"
 task :watir do
   sh "cucumber -p watir features"
 end
+
+desc "Run Kohana Framework tests"
+task :kohana do
+  exec "cd app && phpunit --bootstrap=modules/unittest/bootstrap.php modules/unittest/tests.php"
+end
