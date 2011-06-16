13686990accd8e0b5777efddd2675f4382e7dfa2
Kevin Olbrich <kolbrich@icontact.com>
2011-06-16 06:01:54 -0700

    recipe creates appache virtualhost file, and creates the 'blogs' table

--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -17,30 +17,38 @@ gem_package "mysql" do
   action :install
 end
 
-mysql_database "create application_production database" do
-  host "localhost"
-  username "root"
-  password node[:mysql][:server_root_password]
-  database "application_production"
-  action :create_db
-end
 
-mysql_database "create application_development database" do
-  host "localhost"
-  username "root"
-  password node[:mysql][:server_root_password]
-  database "application_development"
-  action :create_db
-end
+%w{production development test}.each do |env|
+  
+  mysql_database "create application_#{env} database" do
+    host "localhost"
+    username "root"
+    password node[:mysql][:server_root_password]
+    database "application_#{env}"
+    action :create_db
+  end
 
-mysql_database "create application_test database" do
-  host "localhost"
-  username "root"
-  password node[:mysql][:server_root_password]
-  database "application_test"
-  action :create_db
+  execute "create blogs table in application_#{env} database" do
+    sql = %Q{
+      CREATE TABLE IF NOT EXISTS blogs (
+        id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(80) NOT NULL,
+        body TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP);
+    }
+    command "echo \"#{sql}\" | /usr/bin/mysql -u root -p#{node[:mysql][:server_root_password]} application_#{env}"
+  end
+  
 end
 
 link "/var/www/app" do 
   to "/vagrant/app"
 end
+
+web_app "app" do
+  docroot "/var/www/app"
+  template "app.conf.erb"
+  server_name 'localhost'
+end
+
