721e25ad128377335ce8b20b973350fc3c388791
Kevin Olbrich <kolbrich@icontact.com>
2011-06-18 15:28:01 -0400

    add basic crud actions to blog controller

--- app/application/classes/controller/blog.php
+++ app/application/classes/controller/blog.php
@@ -14,6 +14,36 @@ class Controller_Blog extends Controller_Template
     $blogs = ORM::factory('blog')->find_all();
     $this->template->body = View::factory('blog/index')->set('blogs',$blogs);
   }
+  
+  function action_new()
+  {
+      $this->template->title = "Create Blog Entry";
+      $this->template->body = View::factory('blog/new');
+  }
+  
+  function action_create()
+  {
+    $this->auto_render = false;
+    $blog = ORM::factory('blog');
+    $blog->title = $this->request->post('title');
+    $blog->body = $this->request->post('body');
+    $blog->save();
+    $this->request->redirect($this->request->uri(array('action'=>'show', 'id'=>$blog->id)));
+  }
+  
+  function action_edit()
+  {
+      $this->template->title = "Edit Blog Entry";
+      $this->template->body = View::factory('blog/index')->set('blogs',$blogs);
+  }
+  
+  function action_show()
+  {
+      $blog = ORM::factory('blog',intval($this->request->param('id')));
+      // redirect to index if not found
+      $this->template->title = "Blog: $blog->title";
+      $this->template->body = View::factory('blog/show')->set('blog',$blog)->set('request',$this->request);
+  }
 
 }
 
