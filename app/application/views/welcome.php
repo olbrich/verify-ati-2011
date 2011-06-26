3a226859bb7b3e2b941c28d51146a134993e5032
Kevin Olbrich <kolbrich@icontact.com>
2011-06-26 13:45:21 -0400

    add more links

--- app/application/views/welcome.php
+++ app/application/views/welcome.php
@@ -18,4 +18,7 @@
   <li><a href="http://www.opscode.com/chef/">Chef</a></li>
   <li><a href="https://github.com/olbrich/cuke4php">cuke4php</a></li>
   <li><a href="http://gembundler.com/">bundler</a></li>
+  <li><a href="https://github.com/sebastianbergmann/phpunit/">PHPUnit</a></li>
+  <li><a href="https://github.com/jarib/watir-webdriver">Webdriver</a></li>
+  <li><a href="http://relishapp.com/rspec">RSpec</a></li>
 </ul>
