48da7e005386a5565a56c8750c46c72f46f3a95d
Kevin Olbrich <kolbrich@icontact.com>
2011-06-26 15:50:44 -0400

    add support for a nav bar

--- app/application/views/layout/page.php
+++ app/application/views/layout/page.php
@@ -4,10 +4,17 @@
     <link rel="stylesheet" href="/static/main.css" type="text/css" media="screen" title="main" charset="utf-8">
   </head>
   <body>
+    <div id="nav">
+        <ul>
+            <li><?= HTML::anchor('/','Home') ?></li>
+            <?php 
+                if (isset($links)) {
+                foreach ($links as $link) {
+                echo "<li><a href=\"{$link[0]}\">{$link[1]}</a></li>";
+            }}?>
+        </ul>
+    </div>
     <div id="main">
-        <div>
-          <a href="/">Home</a>
-        </div>
         <?php echo $body ?>
     </div>
   </body>
